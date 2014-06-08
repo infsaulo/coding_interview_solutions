f56123112774252f4a66637d38a168bc74b8bfce
Saulo Ricci <sauloricci@Saulos-MacBook-Pro.local>
2014-06-07 21:38:08 -0300

    Adding the ex3 from the chapter 1

---
+++ chapter1/ex3.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+#define AMOUT_ASCII 128
+
+int isPermutation(char *str1, char *str2)
+{
+    int counterCharStr1List[128];
+    int counterCharStr2List[128];
+    int index;
+    for(index=0; index < AMOUT_ASCII; index++)
+    {
+        counterCharStr1List[index] = 0;
+        counterCharStr2List[index] = 0;
+    }
+
+    char *chPtr = str1;
+    while(*chPtr)
+    {
+        counterCharStr1List[(int)*chPtr] += 1;
+        chPtr++;
+    }
+    chPtr = str2;
+    while(*chPtr)
+    {
+        counterCharStr2List[(int)*chPtr] += 1;
+        chPtr++;
+    }
+
+    for(index=0; index < AMOUT_ASCII; index++)
+    {
+        if(counterCharStr1List[index] != counterCharStr2List[index])
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+
+}
+
+int main()
+{
+    char str1[128], str2[128];
+    
+    scanf("%s", str1);
+    scanf("%s", str2);
+
+    if(isPermutation(str1, str2))
+    {
+        printf("Both strings are a permutation of each other\n");
+    }
+
+    else
+    {
+        printf("The strings aren\'t a permutation of each other\n");
+    }
+
+    return 0;
+}
